6b8791fcf431acb749330bc72ca8fe16f8002a45
Adonay Gebrerufael <agebrerufael@miu.edu>
2024-08-06 22:59:31 -0500

    Add customuser and other related changes

--- CourseRegistration/src/main/java/edu/miu/cs/cs425/courseregistration/domain/Faculty.java
+++ CourseRegistration/src/main/java/edu/miu/cs/cs425/courseregistration/domain/Faculty.java
@@ -1,4 +1,33 @@
 package edu.miu.cs.cs425.courseregistration.domain;
+import java.util.List;
 
-public class Faculty {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "faculty")
+public class Faculty extends Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "faculty_id")
+    private Integer id;
+    @OneToMany(mappedBy = "faculty")
+    private List<Lesson> lessons;
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
 }
