a2eefd18bacd79fb104c609b50bccaf75c3583ca
samsom <samsomusa@icloud.com>
2024-08-06 22:58:03 -0500

    FacultySchedule, Lesson, Person, and Stuent

--- CourseRegistration/src/main/java/edu/miu/cs/cs425/courseregistration/domain/Student.java
+++ CourseRegistration/src/main/java/edu/miu/cs/cs425/courseregistration/domain/Student.java
@@ -1,4 +1,44 @@
-package edu.miu.cs.cs425.courseregistration.domain;
+package Project_CourseRegistration.Domain;
 
-public class Student {
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "student")
+public class Student extends Person{
+
+	@ManyToOne
+	@JoinColumn(name="entry_id")
+	private Entry entry;
+	
+	@OneToMany(mappedBy = "student")
+	private List<StudentLesson> studentLessons;
+	
+	@Column
+	private Track track;
+
+	public Entry getEntry() {
+		return entry;
+	}
+
+	public void setEntry(Entry entry) {
+		this.entry = entry;
+	}
+
+	public List<StudentLesson> getStudentLessons() {
+		return studentLessons;
+	}
+
+	public void setStudentLessons(List<StudentLesson> studentLessons) {
+		this.studentLessons = studentLessons;
+	}
+
+	public Track getTrack() {
+		return track;
+	}
+
+	public void setTrack(Track track) {
+		this.track = track;
+	}
 }
